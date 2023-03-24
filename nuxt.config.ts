8946bd957fae368638fa2b92ab647ceef22049a4
chuckbot <cgmilan@gmail.com>
2023-03-24 15:12:45 -0400

    CSS settings and palette color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@vite-pwa/nuxt'],
+  pwa: {},
+  css: ['assets/scss/global.scss']
 })
