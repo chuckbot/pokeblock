f6559986a54c4be348300f1062678afc4c901cc4
chuckbot <cgmilan@gmail.com>
2023-03-24 21:52:52 -0400

    background color to grid cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="container mx-auto">
-    <div class="text-center mb-[3rem] mt-[3rem]">
+    <header class="text-center mb-[3rem] mt-[3rem]">
       <h1>List of Pokemon</h1>
-    </div>
+    </header>
     <div
-      class="grid lg:grid-cols-4 md:grid-cols-3 gap-[3rem] mb-[8rem] max-h-[49rem] overflow-auto 0-8"
+      class="grid lg:grid-cols-4 md:grid-cols-3 gap-[3rem] mb-[8rem] max-h-[50rem] overflow-auto p-8 bg-[var(--tertiary-color)] border-4 border-[var(--secondary-color)] rounded-lg"
     >
       <div v-for="i in 1278">
         <Card />
